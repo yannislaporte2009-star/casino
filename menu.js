5d6c30011a1413aadcc3656d71059d0638b6767f
yannislaporte2009-star <yannis.laporte2009gmail.com>
2026-06-24 09:42:26 +0200

    Remove duplicate variable declaration for slotsBTN in menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,5 @@
 const slotsBTN = document.getElementById('slot-machine');
-const slotsBTN = document.getElementById('slot-machine');
-const slotsBTN = document.getElementById('slot-machine');
+
 
 slotsBTN.addEventListener('click', () => {
   window.location.href = "slots.html";
