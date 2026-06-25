4ec328a0e1f2829ea747f1cf9b9d7c98e2e84d31
yannislaporte2009-star <yannis.laporte2009gmail.com>
2026-06-25 10:50:57 +0200

    del

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-let credits = parseInt(localStorage.getItem('Credits')) || 100;
 
+let credits = parseInt(localStorage.getItem('slotCredits')) || 100;
+ 
 const creditsEl = document.getElementById('credits');
-
+ 
 function updateDisplay() {
   creditsEl.textContent = credits;
 }
-
+ 
 updateDisplay();
 
 const slotsBTN = document.getElementById('slot-machine');
+
+
 slotsBTN.addEventListener('click', () => {
   window.location.href = "slots.html";
 });
+
