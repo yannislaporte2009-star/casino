f9a6a722ac8f0f68d3e922d99e6a5829fe359c21
yannislaporte2009-star <yannis.laporte2009gmail.com>
2026-07-09 13:16:24 +0200

    Update slot symbols and payouts for improved game balance

--- slots.js
+++ slots.js
@@ -1,5 +1,5 @@
-const symbols = ["🎰", "🍒", "🍋", "🍇", "⭐"];
-const payouts = { "🎰": 30, "🍒": 10, "🍋": 10, "🍇": 10, "⭐": 20 };
+const symbols = ["🎰", "🍒", "🍋", "🍇", "🔔"];
+const payouts = { "🎰": 50, "🍒": 10, "🍋": 10, "🍇": 10, "🔔": 30 };
 
 let credits = parseInt(localStorage.getItem('slotCredits')) || 100;
 
